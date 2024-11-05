db7468f655d94f3b7f3e361834efa773888e5329
Aadiaks22 <adityaaks220@gmail.com>
2024-11-05 18:56:17 +0530

    add TanStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.tsx'
@@ -8,8 +9,13 @@ import Modal from 'react-modal';
 // Set the app element for accessibility
 Modal.setAppElement('#root');
 
+// Create a client
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
