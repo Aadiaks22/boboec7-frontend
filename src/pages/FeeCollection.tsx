d5c5b7ecbdacc3aa252d183d13adec0c307af05e
Aadiaks22 <adityaaks220@gmail.com>
2025-02-23 10:46:24 +0530

    Removing Saved reciept tag from printing

--- src/pages/FeeCollection.tsx
+++ src/pages/FeeCollection.tsx
@@ -930,14 +930,14 @@ const FeeCollection = () => {
           {successMessage && <div className="success-message text-green-500 print:hidden ml-4">{successMessage}</div>}
         </div> */}
         {errorMessage && (
-                <Alert variant="destructive" className="mb-4">
+                <Alert variant="destructive" className="print:hidden mb-4">
                   <XCircle className="h-4 w-4" />
                   <AlertTitle>Error</AlertTitle>
                   <AlertDescription>{errorMessage}</AlertDescription>
                 </Alert>
               )}
               {successMessage && (
-                <Alert variant="default" className="mb-4 bg-green-100 text-green-800 border-green-300">
+                <Alert variant="default" className="print:hidden mb-4 bg-green-100 text-green-800 border-green-300">
                   <CheckCircle2 className="h-4 w-4" />
                   <AlertTitle>Success</AlertTitle>
                   <AlertDescription>{successMessage}</AlertDescription>
